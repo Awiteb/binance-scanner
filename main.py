cf2310b3c910f59eab41a40c87d4f5240a15826e
Awiteb <awiteb@hotmail.com>
2021-05-03 14:49:30 +0300

    add while True for scanner

--- main.py
+++ main.py
@@ -39,23 +39,24 @@ def pingCommand(message):
     bot.reply_to(message, text=f"سرعة البوت {typeSpeed}\nالسرعة: {speed} {timeName}\n⁦")
 
 def scanner():
-    for ticker in tickers :
-        timestamp = time.time() - 14400
-        date = datetime.fromtimestamp(timestamp, tz=UTC).strftime("%d%b, %Y %H:%M")
-        try:
-            historical = client.get_historical_klines(symbol=ticker, interval='1h', start_str=date)
-            quote_volumes = [float(i[7]) for i in historical]
-            high = historical[0][2]
-            quote_volume = sum(quote_volumes)
-            if quote_volume >= 50000000:
-                last_hours = '\n'.join([str(i) for i in quote_volumes])
-                text = f"{ticker}\n\n{last_hours}\n\nSum: {quote_volume}\n High: {high}"
-                bot.send_message(chat_id=chat_id, text=text)
-        except IndexError:
-            pass
-        except Exception as e:
-            bot.send_message(chat_id=chat_id, text="ERROR\n\n"+str(e))
-    time.sleep(14400)
+    while True:
+        for ticker in tickers :
+            timestamp = time.time() - 14400
+            date = datetime.fromtimestamp(timestamp, tz=UTC).strftime("%d%b, %Y %H:%M")
+            try:
+                historical = client.get_historical_klines(symbol=ticker, interval='1h', start_str=date)
+                quote_volumes = [float(i[7]) for i in historical]
+                high = historical[3][2]
+                quote_volume = sum(quote_volumes)
+                if quote_volume >= 50000000:
+                    last_hours = '\n'.join([str(round(i, 2)) for i in quote_volumes])
+                    text = f"{ticker}\n\n{last_hours}\n\nSum: {quote_volume}\n High: {high}"
+                    bot.send_message(chat_id=chat_id, text=text)
+            except IndexError:
+                pass
+            except Exception as e:
+                bot.send_message(chat_id=chat_id, text="ERROR\n\n"+str(e))
+        time.sleep(14400)
 
 
 Thread(target=scanner).start()
